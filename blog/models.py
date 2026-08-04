ab0a99604323b9f62eef52021807786589101ad4
Aviral Ale <aviralale@gmail.com>
2026-08-04 13:17:33 +0545

    Add slug field to Post model and implement automatic slug generation; create home and post detail templates

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,25 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     #https://blog.ctrlbits.com/post/software-development-life-cycle-for-2026-developers
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True, blank=True)
     author = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = self.title.lower().replace(' ','-')
+            #My Country Nepal --> my-country-nepal
+            slug = base_slug
+            counter = 1
+            while Post.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+
     def __str__(self):
         return self.title
+    
+    
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse("<h1>Hello my name is aviral</h1>")
+
 def index(request):
-    return HttpResponse("<h1>Hello my name is aviral</h1>")
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def post_detail(request, slug):
+    # post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def about(request):
-    return HttpResponse("<h1>I study at Texas</h1>")
+    return HttpResponse("<h1>I stu`d`y at Texas</h1>")
